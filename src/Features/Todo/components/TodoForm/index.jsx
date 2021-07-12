9d7e1abf559ba492628de9ad14dd62d8e7a045d1
HuuNhat <huunhatth07@gmai.com>
2021-07-13 00:02:22 +0700

    tạo file jsconfig để làm ngắn gọn đường dẫn thu mục từ src. và có thay đổi đường dẫn TodoForm.

--- src/Features/Todo/components/TodoForm/index.jsx
+++ src/Features/Todo/components/TodoForm/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import InputField from '../../../../components/form-controls/InputField';
+// import InputField from '../../../../components/form-controls/InputField';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import InputField from 'components/form-controls/InputField';
 
 
 
