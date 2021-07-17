369f1a51c2b685b2eed3b74f88473e7cb6168b5e
HuuNhat <huunhatth07@gmai.com>
2021-07-17 23:21:08 +0700

    gắn thêm link và naklink và cho các nút trên navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,14 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import {useState} from 'react';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,23 +25,64 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  link:{
+    color: '#fff',
+    textDecoration: 'none',
+  }
 }));
 
 export default function Header() {
   const classes = useStyles();
+
+  const [open, setOpen] =useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
             <GitHubIcon className={classes.menuButton} color="inherit" aria-label="menu"/>
-            <Typography variant="h6" className={classes.title}>
+            <Typography variant='h6' className={classes.title}>
+              <Link to="/" className={classes.link}>
                 ThoundSand
+              </Link>
             </Typography>
-            <Button color="inherit">To do list</Button>
-            <Button color="inherit">Album Music</Button>
-            <Button color="inherit">State Exvercise</Button>
-            <Button color="inherit">Login</Button>
+
+            <NavLink to="/Todo" className={classes.link}>
+              <Button color="inherit">To do list</Button>
+            </NavLink>
+
+            <NavLink to="/AlbumMusic" className={classes.link}>
+              <Button color="inherit">Album Music</Button>
+            </NavLink>
+
+            <NavLink to="/StateExvercise" className={classes.link}>
+              <Button color="inherit">State Exvercise</Button>
+            </NavLink>
+            
+            <Button color="inherit" onClick={handleClickOpen}>Register</Button>
+
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" disableEscapeKeyDown disableBackdropClick>
+            <DialogContent>
+              <DialogContentText>
+                To subscribe to this website, please enter your email address here. We will send updates
+                occasionally.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+            </DialogActions>
+      </Dialog>
           
         </Toolbar>
       </AppBar>
