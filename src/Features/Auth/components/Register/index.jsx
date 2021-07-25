0451c698a71cfa01abd5918f0c0d1bbc3ecf5faa
HuuNhat <huunhatth07@gmai.com>
2021-07-25 23:23:13 +0700

    sử dụng unwrapResult trong redux thunk Api để dispatch action register  đã tạo trước đó.

--- src/Features/Auth/components/Register/index.jsx
+++ src/Features/Auth/components/Register/index.jsx
@@ -1,4 +1,7 @@
+import { unwrapResult } from '@reduxjs/toolkit';
+import { register } from 'Features/Auth/userSlice';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
 
 Register.propTypes = {
@@ -6,8 +9,24 @@ Register.propTypes = {
 };
 
 function Register(props) {
-    const handleSubmit=(values) =>{
+    const dispatch = useDispatch()
+
+    const handleSubmit= async (values) =>{
         console.log('Form Submit', values);
+        
+        try {
+            // auto set username = email
+            values.username = values.email;
+
+            const action = register(values);
+            const resultAction = await dispatch(action);
+            const user = unwrapResult(resultAction);
+            
+            console.log('New user : ', user);
+        } catch (error) {
+            console.log('failded to register', error);
+        }
+        
     }
     return (
         <div>
