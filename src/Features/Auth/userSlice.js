665058f5aabc78eac1a329aa75f7b7e2b2dbfe5a
HuuNhat <huunhatth07@gmai.com>
2021-07-25 22:51:35 +0700

    setup sử dụng redux thunkapi cho userSlice

--- src/Features/Auth/userSlice.js
+++ src/Features/Auth/userSlice.js
@@ -1,4 +1,18 @@
-const {createSlice} = require('@reduxjs/toolkit');
+import { createAsyncThunk,createSlice } from "@reduxjs/toolkit";
+import userApi from "api/userApi";
+
+// First, create the thunk -> sử dụng Redux toolkit ThunkApi 
+const register = createAsyncThunk('users/register',async (payload) => {
+        const data = await userApi.register(payload); 
+      // call api to register
+
+      // save data to localStorage
+        localStorage.setItem('access_token', data.jwt);
+        localStorage.setItem('user', JSON.stringify(data.user));
+
+      return data.user;
+    }
+  )
 
 const userSlice = createSlice({
     name : 'user',
@@ -7,6 +21,13 @@ const userSlice = createSlice({
         settings: {},
     },
     reducers: {},
+    extraReducers: {
+        // action type mình tự định nghĩa
+        // ý nghĩa của dòng bên dưới : user/register/fullfilled
+        [register.fullfilled] : (state, action)=>{
+            state.current = action.payload;
+        }
+    }
 
 });
 
