bf850b353584f8e02dda8371c2a5b1fad5f7fc6c
HuuNhat <huunhatth07@gmai.com>
2021-07-26 23:05:26 +0700

    thêm tính năng đóng form sau khi submit form, và thêm notiStack khi submit form thành công.

--- src/Features/Auth/components/Register/index.jsx
+++ src/Features/Auth/components/Register/index.jsx
@@ -1,15 +1,21 @@
 import { unwrapResult } from '@reduxjs/toolkit';
 import { register } from 'Features/Auth/userSlice';
+import { useSnackbar } from 'notistack';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
+import PropTypes from 'prop-types';
 
 Register.propTypes = {
-    
+    //bài 101 : thêm 1 prop là function để tắt dialog
+    colseDialog: PropTypes.func,
 };
 
 function Register(props) {
     const dispatch = useDispatch()
+
+    // B101 : thêm thông báo notiStack
+    const {enqueueSnackbar} = useSnackbar();
 
     const handleSubmit= async (values) =>{
         console.log('Form Submit', values);
@@ -21,8 +27,19 @@ function Register(props) {
             const action = register(values);
             const resultAction = await dispatch(action);
             const user = unwrapResult(resultAction);
-            
+
+            //bài:101 close dialog
+            const {colseDialog} = props;
+            if (colseDialog) {
+                colseDialog();
+            }
+
+            // do something when user submit
             console.log('New user : ', user);
+
+            //B101: thêm notiStack
+            enqueueSnackbar('Register sussecfully', {variant:'success'});
+
         } catch (error) {
             console.log('failded to register', error);
         }
