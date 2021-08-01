37593d41b174dd868f553b26e482c216f4309a23
HuuNhat <huunhatth07@gmai.com>
2021-08-01 23:43:21 +0700

    bài 106 : tạo nút chuyển đổi giữa register và login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { IconButton } from '@material-ui/core';
+import { Box, IconButton } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,6 +9,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Close } from '@material-ui/icons';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import Login from 'Features/Auth/components/Login';
 import Register from 'Features/Auth/components/Register';
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
@@ -38,7 +39,15 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+  // Bài 106 : tạo 1 hằng số để định nghĩa Login, Register
+  const MODE = {
+    LOGIN:'login',
+    REGISTER:'register'
+  };
+
 export default function Header() {
+  // Bài 106: tạo State để phân biệt khi nào là đăng nhập,đang kí
+  const [mode, setMode] = useState(MODE.LOGIN);
   const classes = useStyles();
 
   const [open, setOpen] =useState(false);
@@ -78,24 +87,42 @@ export default function Header() {
             
             <Button color="inherit" onClick={handleClickOpen}>Register</Button>
 
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" disableEscapeKeyDown>
-              <IconButton className={classes.closeButton} onClick={handleClose}>
-                <Close />
-              </IconButton>
-
-              <DialogContent>
-                <Register colseDialog={handleClose}/>
-              </DialogContent>
-
-              {/* <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                  Cancel
-                </Button>
-              </DialogActions> */}
-            </Dialog>
-          
         </Toolbar>
       </AppBar>
+
+      {/* Khi nhấn vào button Login thì sẽ show ra dialog này */}
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" disableEscapeKeyDown>
+          <IconButton className={classes.closeButton} onClick={handleClose}>
+            <Close />
+          </IconButton>
+
+          <DialogContent>
+            {/* Bài 106 : tạo nút chuyển đổi giữa register và login */}
+            {mode===MODE.REGISTER &&(
+              <>
+              <Register closeDialog={handleClose}></Register>
+              <Box textAlign="center">
+                <Button color="primary" onClick={() => setMode(MODE.LOGIN)}>
+                  Already an account. Login here 
+                </Button>
+              </Box>
+            </>
+            )}
+
+            {mode === MODE.LOGIN && (
+            <>
+              <Login closeDialog={handleClose}></Login>
+              <Box textAlign="center">
+                <Button color="primary" onClick={() => setMode(MODE.REGISTER)}>
+                  Don't have an account. Register here 
+                </Button>
+              </Box>
+            </>
+          )}
+            
+          </DialogContent>
+
+      </Dialog>
     </div>
   );
 }
