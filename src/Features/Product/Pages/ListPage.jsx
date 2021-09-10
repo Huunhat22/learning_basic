8c5b2d355f4d7d5c6c3795cae2de4e9062c64289
HuuNhat <huunhatth07@gmai.com>
2021-09-10 23:42:54 +0700

    Tạo useEffect để nhận Response từ api , và tạo hiệu ứng loading Skeleton.

--- src/Features/Product/Pages/ListPage.jsx
+++ src/Features/Product/Pages/ListPage.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Container, Grid, Paper, makeStyles } from '@material-ui/core';
+import { Box, Container, Grid, Paper, makeStyles, Typography } from '@material-ui/core';
+import productApi from 'api/productApi';
+import ProductSkeletonList from '../components/ProductSkeletonList';
+import ProductList from '../components/ProductList';
 
 ListPage.propTypes = {};
 
@@ -12,12 +15,32 @@ const useStyles = makeStyles((theme) => ({
   },
 
   right: {
-    flex: '1 1 auto',
+    flex: '1 1 0',
   },
 }));
 
 function ListPage(props) {
   const classes = useStyles();
+
+  // Bài 121: tạo state ProductList và isloading
+  const [productList, setProductList] = useState([]);
+  const [Loading, setLoading] = useState(true);
+
+  // tạo UseEffect
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await productApi.getAll({ _page: 1, _limit: 10 });
+        // sử dụng object destructuring
+        setProductList(data);
+        //console.log(response);
+      } catch (error) {
+        console.log('Failed to fetch product list ', error);
+      }
+      // sau khi load productlist xong sẽ tắt loading
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Box>
       <Container>
@@ -27,7 +50,7 @@ function ListPage(props) {
           </Grid>
 
           <Grid item className={classes.right}>
-            <Paper elevation={0}>Colum right</Paper>
+            <Paper elevation={0}>{Loading ? <ProductSkeletonList /> : <ProductList data={productList} />}</Paper>
           </Grid>
         </Grid>
       </Container>
