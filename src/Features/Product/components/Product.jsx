85e556f34235acb6856034c3c9fe59bc7e5d5e2a
HuuNhat <huunhatth07@gmai.com>
2021-09-14 16:29:35 +0700

    Bài 123: Thêm thumbnail cho sản phẩm

--- src/Features/Product/components/Product.jsx
+++ src/Features/Product/components/Product.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
-import { Skeleton } from '@material-ui/lab';
+import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'constants/index';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 Product.propTypes = {
     product: PropTypes.object,
 };
 
 function Product({ product }) {
+    // HẰNG SỐ ĐƯỢC IMPORT VÀO TỪ common.js thông qua index.js
+    const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
     return (
         <Box padding={1}>
-            <Skeleton variant="rect" width="100%" height={118} />
+            {/* Thumbnail cho sản phẩm */}
+            <Box padding={1}>
+                <img src={thumbnailUrl} alt={product.name} width="100%" />
+            </Box>
+
             <Typography>{product.name}</Typography>
             <Typography width="60%">
-                {product.salePrice} - {product.promotionPercent}
+                <Box component={'span'} fontWeight="bold" mr={1}>
+                    {/* intl NumberFormat */}
+                    {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
+                </Box>
+
+                {/* nếu lớn hơn 0 thì hiển thị phần trăm, ngược lại thì ko hiển thị */}
+                {product.promotionPercent > 0 ? ` -${product.promotionPercent}%` : ''}
             </Typography>
         </Box>
     );
