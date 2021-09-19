33b838c250200978fbc41c87ba25dd4a05eda92d
HuuNhat <huunhatth07@gmai.com>
2021-09-19 22:48:10 +0700

    set minHeight cho ảnh, để khi load nó không bị giật

--- src/Features/Product/components/Product.jsx
+++ src/Features/Product/components/Product.jsx
@@ -4,31 +4,31 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 Product.propTypes = {
-    product: PropTypes.object,
+  product: PropTypes.object,
 };
 
 function Product({ product }) {
-    // HẰNG SỐ ĐƯỢC IMPORT VÀO TỪ common.js thông qua index.js
-    const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
-    return (
-        <Box padding={1}>
-            {/* Thumbnail cho sản phẩm */}
-            <Box padding={1}>
-                <img src={thumbnailUrl} alt={product.name} width="100%" />
-            </Box>
+  // HẰNG SỐ ĐƯỢC IMPORT VÀO TỪ common.js thông qua index.js
+  const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
+  return (
+    <Box padding={1}>
+      {/* Thumbnail cho sản phẩm */}
+      <Box padding={1}>
+        <img src={thumbnailUrl} alt={product.name} width="100%" minHeight={200} />
+      </Box>
 
-            <Typography>{product.name}</Typography>
-            <Typography width="60%">
-                <Box component={'span'} fontWeight="bold" mr={1}>
-                    {/* intl NumberFormat */}
-                    {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
-                </Box>
-
-                {/* nếu lớn hơn 0 thì hiển thị phần trăm, ngược lại thì ko hiển thị */}
-                {product.promotionPercent > 0 ? ` -${product.promotionPercent}%` : ''}
-            </Typography>
+      <Typography>{product.name}</Typography>
+      <Typography width="60%">
+        <Box component={'span'} fontWeight="bold" mr={1}>
+          {/* intl NumberFormat */}
+          {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
         </Box>
-    );
+
+        {/* nếu lớn hơn 0 thì hiển thị phần trăm, ngược lại thì ko hiển thị */}
+        {product.promotionPercent > 0 ? ` -${product.promotionPercent}%` : ''}
+      </Typography>
+    </Box>
+  );
 }
 
 export default Product;
