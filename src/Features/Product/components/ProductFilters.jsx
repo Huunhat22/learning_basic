9705e64377476751afde6859e44722ef94627fce
HuuNhat <huunhatth07@gmai.com>
2021-10-06 00:05:56 +0700

    Bài 133 135 : thêm 2 component FilterByPrice và FilterByService, và handle change cho 2 component này.

--- src/Features/Product/components/ProductFilters.jsx
+++ src/Features/Product/components/ProductFilters.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core';
 import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
+import FilterByService from './Filters/FilterByService';
 
 
 ProductFilters.propTypes = {
@@ -24,9 +26,30 @@ function ProductFilters({ filters, onChange }) {
         onChange(newFilters);
     }
 
+    // Bài 133: hàm này sẽ handle khi filterPrice thay đổi, nhận vào 1 object chưa 2 giá trị là gte và lte
+    const handlePriceChange = (values) => {
+        // console.log(values);
+        if (onChange) {
+            onChange(values)
+        }
+    };
+
+    // bài 135
+    const handelServiceChange = (values) => {
+        if (onChange) {
+            onChange(values)
+        }
+    };
+
     return (
         <Box>
             <FilterByCategory onChange={handleCategoryChange} />
+
+            {/* Bài 133: FilterByPrice */}
+            <FilterByPrice onChange={handlePriceChange} />
+
+            {/* Bài 135: FilterByService */}
+            <FilterByService filters={filters} onChange={handelServiceChange} />
         </Box>
     );
 }
