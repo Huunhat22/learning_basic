8951cbd5a3803209bfd68271c26831ee7b57f46a
huunhat22 <huunhatth07@gmail.com>
2021-11-11 23:49:59 +0700

    Bài 145:  sử dụng hook đã customize , trả về 2 tham sô là product và loading. và truyền vào props của component ProductThumbnail

--- src/Features/Product/Pages/DetailPage.jsx
+++ src/Features/Product/Pages/DetailPage.jsx
@@ -1,6 +1,8 @@
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
+import { useRouteMatch } from 'react-router';
 import ProducThumbnail from '../components/ProducThumbnail';
+import useProductDetail from '../hook/useProductDetail';
 
 DetailPage.propTypes = {
     
@@ -24,15 +26,30 @@ const useStyles = makeStyles((theme) => ({
 
 
 function DetailPage(props) {
+
     const classes = useStyles();
+
+    // const match = useRouteMatch();
+    // console.log({match});
+
+    const { params :{productId},} = useRouteMatch();
+
+    // Bài 145: Tạo 1 custom hook thể lấy dữ liệu
+    const {product, loading} = useProductDetail(productId);
+
+    if(loading){
+        return <Box>
+                    Loading
+                </Box>
+    }
 
     return (
         <Box className={classes.root}>
             <Container>
                 <Paper elevation={0}>
-                    <Grid container clas>
+                    <Grid container>
                         <Grid item className={classes.left}>
-                            <ProducThumbnail product={{}}/>
+                            <ProducThumbnail product={product}/>
                         </Grid>
                         <Grid item className={classes.right}>
                             Detail Info
