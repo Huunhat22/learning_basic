71e43d9f7aa9e12eef5d47d96996d982be172ffa
huunhat22 <huunhatth07@gmail.com>
2021-11-29 10:48:36 +0700

    Tạo component Cart

---
+++ src/Features/Cart/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+CartFeatur.propTypes = {
+    
+};
+
+function CartFeatur(props) {
+    return (
+        <div>
+            CartFeatur
+        </div>
+    );
+}
+
+export default CartFeatur;
