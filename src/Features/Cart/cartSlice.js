ca1768870c2bfaa550257276af66a0d77e81d043
HuuNhat <huunhatth07@gmai.com>
2021-12-01 21:58:55 +0700

    Bài 156: Tạo các action cho cartslice

--- src/Features/Cart/cartSlice.js
+++ src/Features/Cart/cartSlice.js
@@ -1,24 +1,55 @@
-const {createSlice} = require('@reduxjs/toolkit');
+const { createSlice } = require('@reduxjs/toolkit');
 
 const cartSlice = createSlice({
-    name : 'cart',
-    initialState : {
+    name: 'cart',
+    initialState: {
         showMiniCart: false,
 
         // cartItems là mảng các giá trị: productID,product, quantity
-        cartItems:[],
+        cartItems: [],
     },
-    reducers:{
-        showMiniCart(state){
-            return state.showMiniCart= true;
+    reducers: {
+        showMiniCart(state) {
+            return state.showMiniCart = true;
         },
 
-        hideMiniCart(state){
+        hideMiniCart(state) {
             return state.showMiniCart = false;
         },
+
+        addToCard(state, action) {
+            // newItem = { id,product, quantity };
+            const newItem = action.payload;
+
+            const index = state.cartItems.findIndex((x) => x.id === newItem.id);
+            // if already exist item in cart then update quantity
+            if (index >= 0) {
+                state.cartItems[index].quantity += newItem.quantity;
+            }
+            //  add new product to cart
+            else {
+                state.cartItems.push(newItem);
+            }
+        },
+
+        removeCardItem(state, action) {
+            const idNeedRemove = action.payload;
+            state.cartItems = state.cartItems.filter((x) => x.id !== idNeedRemove);
+        },
+
+        setQuantityItem(state, action) {
+            const { id, quantity } = action.payload;
+
+            // check if product is available in cart
+            const index = state.cartItems.findIndex((x) => x.id === id);
+            if (index >= 0) {
+                state.cartItems[index].quantity = quantity;
+            }
+        }
+
     },
 });
 
-const {actions,reducer} = cartSlice;
-export const {showMiniCart,hideMiniCart} = actions; //định nghĩa bằng name export
+const { actions, reducer } = cartSlice;
+export const { showMiniCart, hideMiniCart, addToCard, removeCardItem, setQuantityItem } = actions; //định nghĩa bằng name export
 export default reducer; // default export
