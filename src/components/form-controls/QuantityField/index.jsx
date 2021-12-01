9176e0684cce527686d62c596f9d88ea909f27fd
HuuNhat <huunhatth07@gmai.com>
2021-12-01 21:58:05 +0700

    Update : thêm css cho thẻ input

--- src/components/form-controls/QuantityField/index.jsx
+++ src/components/form-controls/QuantityField/index.jsx
@@ -1,4 +1,4 @@
-import { Box, FormHelperText, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Box, FormHelperText, IconButton, Input, makeStyles, Typography } from '@material-ui/core';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
@@ -10,58 +10,65 @@ import { Controller } from 'react-hook-form';
 
 QuantityField.propTypes = {
     form: PropTypes.object.isRequired,
-    name : PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
 
-    label : PropTypes.string,
-    disabled : PropTypes.bool,
+    label: PropTypes.string,
+    disabled: PropTypes.bool,
 };
 
-const useStyle = makeStyles((theme) =>({
-    root:{},
-    quantity:{
+const useStyle = makeStyles((theme) => ({
+    root: {
+    },
+    quantity: {
         fontWeight: 'bold',
-        marginBottom:theme.spacing(2),
-    }
+        marginBottom: theme.spacing(2),
+    },
+
+    textfield: {
+        "& input": {
+            textAlign: 'center',
+        }
+    },
 }))
 
 function QuantityField(props) {
     const classes = useStyle();
 
-    const {form,name,label} = props;
-    const {control,setValue} = form;
+    const { form, name, label } = props;
+    const { control, setValue } = form;
 
     // bài 149: handleRemoveQuantity
-    const handleRemoveQuantity = (name,value) =>{
-        setValue(name,Number.parseInt(value) ? Number.parseInt(value) - 1 : 1)
+    const handleRemoveQuantity = (name, value) => {
+        setValue(name, Number.parseInt(value) ? Number.parseInt(value) - 1 : 1)
     }
 
-    const handleAddQuantity = (name,value) =>{
-        setValue(name,Number.parseInt(value) ? Number.parseInt(value) + 1 : 1)
+    const handleAddQuantity = (name, value) => {
+        setValue(name, Number.parseInt(value) ? Number.parseInt(value) + 1 : 1)
     }
 
     return (
         // Input Field -> sẽ tự dộng bind
-            <Controller
-                name={name}
-                control={control}
-                render={({field : {onChange,onBlur,value,name},fieldState: {invalid,error}}) =>(
-                    <>
+        <Controller
+            name={name}
+            control={control}
+            render={({ field: { onChange, onBlur, value, name }, fieldState: { invalid, error } }) => (
+                <>
                     <FormControl error={invalid} fullWidth variant="outlined" margin="normal">
                         <Typography className={classes.quantity}>{label}</Typography>
 
                         <Box>
-                            <IconButton onClick={() => handleRemoveQuantity(name,value)}>
-                                <RemoveCircleOutline/>
+                            <IconButton onClick={() => handleRemoveQuantity(name, value)}>
+                                <RemoveCircleOutline />
                             </IconButton>
-                            <OutlinedInput
+                            <OutlinedInput className={classes.textfield}
                                 id={name}
                                 error={invalid}
                                 type='quantity'
-                                value = {value}
-                                onChange = {onChange}
-                                onBlur = {onBlur}
+                                value={value}
+                                onChange={onChange}
+                                onBlur={onBlur}
                             />
-                            <IconButton onClick={() => handleAddQuantity(name,value)}>
+                            <IconButton onClick={() => handleAddQuantity(name, value)}>
                                 <AddCircleOutline></AddCircleOutline>
                             </IconButton>
                         </Box>
@@ -69,10 +76,10 @@ function QuantityField(props) {
                     </FormControl>
                     {/* Lưu ý khi validate field phải thêm FormHelperText */}
                     <FormHelperText error={invalid}>{error?.message}</FormHelperText>
-                    </>
-                )}   
-            ></Controller>    
-   
+                </>
+            )}
+        ></Controller>
+
     );
 }
 
