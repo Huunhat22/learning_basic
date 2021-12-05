dab15eb616326ba76f63091afbd47c0bcf7e2666
HuuNhat <huunhatth07@gmai.com>
2021-12-05 22:56:03 +0700

    Bài 159: sử dụng useSelector đã viết trước đó để hiển thị tổng tiền của giỏ hàng.

--- src/Features/Cart/index.jsx
+++ src/Features/Cart/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { cartTotalSelector } from './selectors';
 
 CartFeatur.propTypes = {
-    
+
 };
 
 function CartFeatur(props) {
+
+    // Bài 159: lấy ra tổng số tiền của giỏ hàng
+    const totalCart = useSelector(cartTotalSelector);
+
     return (
         <div>
-            CartFeatur
+            CartFeatur {totalCart}
         </div>
     );
 }
