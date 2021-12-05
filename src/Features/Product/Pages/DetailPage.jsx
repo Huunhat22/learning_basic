56a630337a27c10ac2c2b31893198978e3a1285d
HuuNhat <huunhatth07@gmai.com>
2021-12-05 22:52:07 +0700

    Bài 158: HandleAddToCardForm khi onSubmit. Sử dụng Hook useDispatch để dispatch action lên redux.

--- src/Features/Product/Pages/DetailPage.jsx
+++ src/Features/Product/Pages/DetailPage.jsx
@@ -9,33 +9,36 @@ import useProductDetail from '../hook/useProductDetail';
 import ProductDescription from '../components/ProductDescription';
 import ProductAdditional from '../components/ProductAdditional';
 import ProductReviews from '../components/ProductReviews';
+import { useDispatch } from 'react-redux';
+import { addToCard } from 'Features/Cart/cartSlice';
+
 
 DetailPage.propTypes = {
-    
+
 };
 
 const useStyles = makeStyles((theme) => ({
     root: {
     },
-  
+
     left: {
-      width: '400px',
-      padding: theme.spacing(1.5),
-      borderRight: `1px solid ${theme.palette.grey[300]}`
+        width: '400px',
+        padding: theme.spacing(1.5),
+        borderRight: `1px solid ${theme.palette.grey[300]}`
     },
-  
+
     right: {
         flex: '1 1 0',
         padding: theme.spacing(1.5),
     },
 
-    progress:{
-        position:'fixed',
-        top:0,
-        left:0,
-        width:'100%',
+    progress: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
     }
-  }));
+}));
 
 
 function DetailPage(props) {
@@ -44,23 +47,35 @@ function DetailPage(props) {
 
     // const match = useRouteMatch();
     // console.log({match});
-    
+
     // Bài 151: thêm url sử dụng RouteMatch
-    const { params :{productId},url} = useRouteMatch();
+    const { params: { productId }, url } = useRouteMatch();
 
     // Bài 145: Tạo 1 custom hook thể lấy dữ liệu
-    const {product, loading} = useProductDetail(productId);
+    const { product, loading } = useProductDetail(productId);
 
-    if(loading){
+    // Bài 158: để dispatch một action thì sử dụng 1 hook useDispatch()
+    const dispatch = useDispatch();
+
+    if (loading) {
         return <Box className={classes.progress}>
-                    {/* Bài 152: thêm LinearProgress khi fetch data */}
-                    <LinearProgress  color="secondary"/>
-                </Box>
+            {/* Bài 152: thêm LinearProgress khi fetch data */}
+            <LinearProgress color="secondary" />
+        </Box>
     }
 
     // Bài 148: Handle Add To Card Form Submit
-    const HandleAddToCardForm = (formValues) =>{
-        console.log('Fomr Submit',formValues);
+    const HandleAddToCardForm = (formValues) => {
+        // console.log('Fomr Submit',formValues);
+
+        // Bài 158 : handle addToCart
+        const action = addToCard({
+            id: product.id,
+            product,
+            quantity: formValues.quantity,
+
+        });
+        dispatch(action);
     }
 
     return (
@@ -69,11 +84,11 @@ function DetailPage(props) {
                 <Paper elevation={0}>
                     <Grid container>
                         <Grid item className={classes.left}>
-                            <ProducThumbnail product={product}/>
+                            <ProducThumbnail product={product} />
                         </Grid>
                         <Grid item className={classes.right}>
-                            <ProductInfo product={product}/>
-                            <AddToCardForm onSubmit={HandleAddToCardForm}/>
+                            <ProductInfo product={product} />
+                            <AddToCardForm onSubmit={HandleAddToCardForm} />
                         </Grid>
                     </Grid>
                 </Paper>
@@ -82,7 +97,7 @@ function DetailPage(props) {
                 {/* Bài 151: render nội dung theo link, tại một thời điểm thì chỉ match 1 link thì sử dụng switch -> route */}
                 <Switch>
                     <Route path={url} exact>
-                        <ProductDescription  product={product}/>
+                        <ProductDescription product={product} />
                     </Route>
 
                     <Route path={`${url}/additional`} exact component={ProductAdditional}></Route>
