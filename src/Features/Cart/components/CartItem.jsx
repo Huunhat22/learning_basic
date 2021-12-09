37dc895474adeeb96921fe4adc97504c092d4849
huunhat22 <huunhatth07@gmail.com>
2021-12-10 00:06:34 +0700

    render các item có trong CartList.

--- src/Features/Cart/components/CartItem.jsx
+++ src/Features/Cart/components/CartItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Box, Paper, Typography } from '@material-ui/core';
 
 CartItem.propTypes = {
-    
+    product: PropTypes.object,
 };
 
-function CartItem(props) {
+function CartItem({product={}}) {
+
+    const {name} = product;
+
     return (
-        <div>
-            
-        </div>
+        <Paper elevation={0}>
+            <Typography>{name}</Typography>
+        </Paper>
     );
 }
 
