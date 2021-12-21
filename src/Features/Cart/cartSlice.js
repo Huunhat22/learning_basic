2967aefa808ca3b9a211ace3781a7601b87a67a6
huunhat22 <huunhatth07@gmail.com>
2021-12-22 00:18:21 +0700

    format with pertier

--- src/Features/Cart/cartSlice.js
+++ src/Features/Cart/cartSlice.js
@@ -1,53 +1,52 @@
 const { createSlice } = require('@reduxjs/toolkit');
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        showMiniCart: false,
+  name: 'cart',
+  initialState: {
+    showMiniCart: false,
 
-        // cartItems là mảng các giá trị: productID,product, quantity
-        cartItems: [],
+    // cartItems là mảng các giá trị: productID,product, quantity
+    cartItems: [],
+  },
+  reducers: {
+    showMiniCart(state) {
+      return (state.showMiniCart = true);
     },
-    reducers: {
-        showMiniCart(state) {
-            return state.showMiniCart = true;
-        },
 
-        hideMiniCart(state) {
-            return state.showMiniCart = false;
-        },
-
-        addToCard(state, action) {
-            // newItem = { id,product, quantity };
-            const newItem = action.payload;
-
-            const index = state.cartItems.findIndex((x) => x.id === newItem.id);
-            // if already exist item in cart then update quantity
-            if (index >= 0) {
-                state.cartItems[index].quantity += newItem.quantity;
-            }
-            //  add new product to cart
-            else {
-                state.cartItems.push(newItem);
-            }
-        },
-
-        removeCardItem(state, action) {
-            const idNeedRemove = action.payload;
-            state.cartItems = state.cartItems.filter((x) => x.id !== idNeedRemove);
-        },
-
-        setQuantityItem(state, action) {
-            const { id, quantity } = action.payload;
-
-            // check if product is available in cart
-            const index = state.cartItems.findIndex((x) => x.id === id);
-            if (index >= 0) {
-                state.cartItems[index].quantity = quantity;
-            }
-        }
-
+    hideMiniCart(state) {
+      return (state.showMiniCart = false);
     },
+
+    addToCard(state, action) {
+      // newItem = { id,product, quantity };
+      const newItem = action.payload;
+
+      const index = state.cartItems.findIndex((x) => x.id === newItem.id);
+      // if already exist item in cart then update quantity
+      if (index >= 0) {
+        state.cartItems[index].quantity += newItem.quantity;
+      }
+      //  add new product to cart
+      else {
+        state.cartItems.push(newItem);
+      }
+    },
+
+    removeCardItem(state, action) {
+      const idNeedRemove = action.payload;
+      state.cartItems = state.cartItems.filter((x) => x.id !== idNeedRemove);
+    },
+
+    setQuantityItem(state, action) {
+      const { id, quantity } = action.payload;
+
+      // check if product is available in cart
+      const index = state.cartItems.findIndex((x) => x.id === id);
+      if (index >= 0) {
+        state.cartItems[index].quantity = quantity;
+      }
+    },
+  },
 });
 
 const { actions, reducer } = cartSlice;
