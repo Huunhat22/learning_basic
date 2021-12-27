5295ec8fed3d9eb6d8c3cc7dd7d2664544bf3f28
HuuNhat <huunhatth07@gmai.com>
2021-12-27 17:35:14 +0700

    format code

--- src/Features/Cart/components/CartList.jsx
+++ src/Features/Cart/components/CartList.jsx
@@ -4,26 +4,26 @@ import React from 'react';
 import CartItem from './CartItem';
 
 CartList.propTypes = {
-    data:PropTypes.array,
+    data: PropTypes.array,
 };
 
 const useStyles = makeStyles((theme) => ({
-    root:{},
-    CartList:{
-        listStyle:'none',
-        margin:'0',
-        padding:'0',
+    root: {},
+    CartList: {
+        listStyle: 'none',
+        margin: '0',
+        padding: '0',
     },
-    CartItem:{
-        margin:theme.spacing(2 ,0),
+    CartItem: {
+        margin: theme.spacing(2, 0),
     },
 }));
 
-function CartList({data=[]}) {
+function CartList({ data = [] }) {
     const classes = useStyles();
     return (
         <Box component='ul' className={classes.CartList}>
-            {data.map((item)=> (
+            {data.map((item) => (
                 <li item key={item.id} className={classes.CartItem}>
                     <CartItem item={item}></CartItem>
                 </li>
